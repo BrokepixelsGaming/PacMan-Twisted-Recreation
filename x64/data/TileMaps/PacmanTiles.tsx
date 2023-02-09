f31ea6772ec666ce7dbe7d5964126208f65d12f9
BrokepixelsGaming <71676028+BrokepixelsGaming@users.noreply.github.com>
2023-02-09 10:19:15 -0800

    Add files via upload

---
+++ x64/data/TileMaps/PacmanTiles.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="PacmanTiles" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="pacman tiles.png" width="48" height="32"/>
+ <tile id="0" type="wall">
+  <properties>
+   <property name="weight" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1" type="smallOrb">
+  <properties>
+   <property name="weight" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2" type="largeOrb">
+  <properties>
+   <property name="weight" type="float" value="5"/>
+  </properties>
+ </tile>
+ <tile id="3" type="emptyPath">
+  <properties>
+   <property name="weight" type="float" value="10"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="weight" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="weight" type="float" value="0"/>
+  </properties>
+ </tile>
+</tileset>
